969abab4b9370927dca1d3459c6674d2042af65f
sumairq <sumairqaisar992@gmail.com>
2024-01-20 16:02:29 +0500

    Add Header and footer layout

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,6 +1,10 @@
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { Button } from '../ui/button'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
 
 const Header = () => {
   return (
@@ -9,8 +13,24 @@ const Header = () => {
             <Link href="/" className='w-36'>
                 <Image src="/assets/images/logo.svg" width={128} height={38} alt="Evently logo"/>
             </Link>
+
+            <SignedIn>
+                <nav className='md:flex-between hidden w-full max-w-xs'>
+                    <NavItems/>
+                </nav>
+            </SignedIn>
             <div className='flex w-32 justify-end gap-3'>
-           
+                <SignedIn>
+                    <UserButton afterSignOutUrl='/'/>
+                    <MobileNav/>
+                </SignedIn>
+           <SignedOut>
+            <Button asChild className='rounded-full' size="lg">
+                <Link href={"/sign-in"}>
+                login   
+                </Link>
+            </Button>
+           </SignedOut>
             </div>
         </div>
     </header>
