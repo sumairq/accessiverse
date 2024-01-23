4c0ec9517f0cea5f4078b543eef50eb01bb39549
sumairq <sumairqaisar992@gmail.com>
2024-01-23 20:24:09 +0500

    Implement basic profile page

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -35,8 +35,8 @@ const Card = ({event, hasOrderLink, hidePrice}: CardProps) => {
                 <DeleteConfirmation eventId={event._id}/>
             </div>
         )}
-        <Link 
-        href={`/events/${event._id}`}
+        <div 
+       
         className='flex min-h-[230px] flex-col gap-3 p-5 md:gap-4'
         >
            {!hidePrice && <div className="flex gap-2">
@@ -54,9 +54,10 @@ const Card = ({event, hasOrderLink, hidePrice}: CardProps) => {
             <p className='p-medium-16 p-medium-18 text-grey-500'>
                 {formatDateTime(event.startDateTime).dateTime}
                 </p>
-
+                <Link  href={`/events/${event._id}`}>
                 <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">{event.title}
                 </p>
+                </Link>
 
                 <div className="flex-between w-full">
                   <p className="p-medium-14 md:p-medium-16 text-grey-600">
@@ -71,7 +72,7 @@ const Card = ({event, hasOrderLink, hidePrice}: CardProps) => {
                   )}
                 </div>
 
-        </Link>
+        </div>
         </div>
   )
 }
--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -10,7 +10,7 @@ type CollectionProps = {
     page: number | string,
     totalPages?: number,
     urlParamName?: string,
-    collectionType?: 'Events_Organized' | 'My_Events' | 'All_Events',
+    collectionType?: 'Events_Organized' | 'My_Tickets' | 'All_Events',
 }
 
 const Collection = ({
