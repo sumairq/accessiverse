716aa9375f8b1248d5dad0aafb12486b0ac1f1e6
sumairq <sumairqaisar992@gmail.com>
2024-01-24 13:02:26 +0500

    Implement buy ticket button

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -4,6 +4,7 @@ import { getEventById, getRelatedEventsByCategory } from '@/lib/actions/event.ac
 import Image from 'next/image';
 import { formatDateTime } from '@/lib/utils';
 import Collection from '@/components/shared/Collection';
+import CheckoutButton from '@/components/shared/CheckoutButton';
 
 const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) => {
   const event = await getEventById(id);
@@ -49,6 +50,8 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
             </div>
           </div>
           {/* CHECKOUT BUTTON */}
+          <CheckoutButton event={event}/>
+
 
           <div className="flex flex-col gap-5">
             <div className="flex gap-2 md:gap-3">
